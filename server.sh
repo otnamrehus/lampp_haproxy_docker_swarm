85f7e39e91b0f658713b528e74652064ddbcf8aa
otnamrehus <97101355+otnamrehus@users.noreply.github.com>
2023-12-08 22:03:37 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 clear
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -12,27 +13,26 @@ echo "c - [?] Cek Status Stack Swarm "
 echo "-------------------------------------------------------------------"
 echo "ss - [?] Status Service Swarm Berjalan "
 echo "-------------------------------------------------------------------"
-echo "r - [X] Hapus Stack Lampp_Swarm  " 
+echo "r - [X] Hapus Stack Lampp_Swarm  "
 echo "-------------------------------------------------------------------"
 echo "rr - [!] Hentikan Docker Swarm [Hapus Seluruh Stack yang berjalan] "
 echo "-------------------------------------------------------------------"
 echo "Opsi Perintah Tambahan :"
-echo "1. Manager Node: " 
-echo "   docker swarm  init     ## Default Manager "
-echo "   docker swarm join-token manager     ## making Manager "
-echo "2. Join Node: " 
-echo "   docker swarm join --token <TOKEN> <IP>:<PORT>    ## Gabung Ke Noder" 
+echo "1. Manager Node: "
+echo "   docker swarm  init      "
+echo "   docker swarm join-token manager      "
+echo "2. Join Node: "
+echo "   docker swarm join --token <TOKEN> <IP>:<PORT>    "
 echo "-------------------------------------------------------------------"
 echo " "
 
 read swarm;
 case $swarm in
-  s) docker swarm  init ;;
-  d) docker stack deploy -c docker-compose.yaml lampp_swarm ;;
-  c) docker stack ps lampp_swarm ;;
-  ss) docker service ls ;;  
-  r) docker stack rm lampp_swarm ;;
-  rr) docker swarm leave --force ;;
+  s) docker swarm init && ./server.sh;;
+  d) docker stack deploy -c docker-compose.yaml lampp_swarm && ./server.sh;;
+  c) docker stack ps lampp_swarm && ./server.sh;;
+  ss) docker service ls && ./server.sh;;
+  r) docker stack rm lampp_swarm && ./server.sh;;
+  rr) docker swarm leave --force && ./server.sh;;
   *) clear && echo "Keluar dari Pilihan Service Docker Swarm";;
 esac
-####################### E N D #########################
